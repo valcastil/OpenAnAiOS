8e71d83c13bb9c4bda099319cef00ea49794346b
valcastil <castilv@gmail,com>
2026-05-17 16:47:52 +0400

    change address

--- constants/index.js
+++ constants/index.js
@@ -1,183 +1,183 @@
 const navLinks = [
- {
-	id: "cocktails",
-	title: "Apps and Ai Cocktails",
- },
- {
-	id: "about",
-	title: "About Us",
- },
- {
-	id: "art",
-	title: "The Art",
- },
- {
-	id: "contact",
-	title: "Contact",
- },
+	{
+		id: "cocktails",
+		title: "Apps and Ai Cocktails",
+	},
+	{
+		id: "about",
+		title: "About Us",
+	},
+	{
+		id: "art",
+		title: "The Art",
+	},
+	{
+		id: "contact",
+		title: "Contact",
+	},
 ];
 
 const cocktailLists = [
- {
-	name: "iOS and Android Apps",
-	country: "Development",
-	detail: "With Ai",
-	price: "",
- },
- {
-	name: "Ai Automation",
-	country: "Office",
-	detail: "Solutions",
-	price: "",
- },
- {
-	name: "n8n Workflows",
-	country: "Automation",
-	detail: "Ai Assisted",
-	price: "",
- },
- {
-	name: "GHL, Zapier or Make.com",
-	country: "Integrations",
-	detail: "Provider",
-	price: "",
- },
+	{
+		name: "iOS and Android Apps",
+		country: "Development",
+		detail: "With Ai",
+		price: "",
+	},
+	{
+		name: "Ai Automation",
+		country: "Office",
+		detail: "Solutions",
+		price: "",
+	},
+	{
+		name: "n8n Workflows",
+		country: "Automation",
+		detail: "Ai Assisted",
+		price: "",
+	},
+	{
+		name: "GHL, Zapier or Make.com",
+		country: "Integrations",
+		detail: "Provider",
+		price: "",
+	},
 ];
 
 const mockTailLists = [
- {
-	name: "WhatsApp Ai Assistant",
-	country: "n8n",
-	detail: "OpenAi",
-	price: "",
- },
- {
-	name: "Office Documentation RAG",
-	country: "Pinecone",
-	detail: "OpenAi",
-	price: "",
- },
- {
-	name: "Personalization",
-	country: "Ai Agent",
-	detail: "Self-help",
-	price: "",
- },
- {
-	name: "Web Apps",
-	country: "Static Sites",
-	detail: "Funnels",
-	price: "",
- },
+	{
+		name: "WhatsApp Ai Assistant",
+		country: "n8n",
+		detail: "OpenAi",
+		price: "",
+	},
+	{
+		name: "Office Documentation RAG",
+		country: "Pinecone",
+		detail: "OpenAi",
+		price: "",
+	},
+	{
+		name: "Personalization",
+		country: "Ai Agent",
+		detail: "Self-help",
+		price: "",
+	},
+	{
+		name: "Web Apps",
+		country: "Static Sites",
+		detail: "Funnels",
+		price: "",
+	},
 ];
 
 const profileLists = [
- {
-	imgPath: "/images/profile1.png",
- },
- {
-	imgPath: "/images/profile2.png",
- },
- {
-	imgPath: "/images/profile3.png",
- },
- {
-	imgPath: "/images/profile4.png",
- },
+	{
+		imgPath: "/images/profile1.png",
+	},
+	{
+		imgPath: "/images/profile2.png",
+	},
+	{
+		imgPath: "/images/profile3.png",
+	},
+	{
+		imgPath: "/images/profile4.png",
+	},
 ];
 
 const featureLists = [
- "Perfectly balanced Ai Agents",
- "Trained to perfection",
- "Available every time",
- "Expertly prompted & verified",
+	"Perfectly balanced Ai Agents",
+	"Trained to perfection",
+	"Available every time",
+	"Expertly prompted & verified",
 ];
 
 const goodLists = [
- "Handpicked Nodes",
- "Signature Workflows",
- "Automation artistry in action",
- "Freshly selected models",
+	"Handpicked Nodes",
+	"Signature Workflows",
+	"Automation artistry in action",
+	"Freshly selected models",
 ];
 
 const storeInfo = {
- heading: "Where to Find Us",
- address: "Dubai, UAE",
- contact: {
-	phone: "+971 52 608 1614",
-	email: "support@openanaios.com",
- },
+	heading: "Where to Find Us",
+	address: "Lapu-lapu City, Cebu, Philippines",
+	contact: {
+		phone: "+63 966 073 8395",
+		email: "support@openanaios.com",
+	},
 };
 
 const openingHours = [
- { day: "Mon–Thu", time: "11:00am – 12am" },
- { day: "Fri", time: "11:00am – 2am" },
- { day: "Sat", time: "9:00am – 2am" },
- { day: "Sun", time: "9:00am – 1am" },
+	{ day: "Mon–Thu", time: "11:00am – 12am" },
+	{ day: "Fri", time: "11:00am – 2am" },
+	{ day: "Sat", time: "9:00am – 2am" },
+	{ day: "Sun", time: "9:00am – 1am" },
 ];
 
 const socials = [
- {
-	name: "Instagram",
-	icon: "/images/insta.png",
-	url: "#",
- },
- {
-	name: "X (Twitter)",
-	icon: "/images/x.png",
-	url: "#",
- },
- {
-	name: "Facebook",
-	icon: "/images/fb.png",
-	url: "#",
- },
+	{
+		name: "Instagram",
+		icon: "/images/insta.png",
+		url: "#",
+	},
+	{
+		name: "X (Twitter)",
+		icon: "/images/x.png",
+		url: "#",
+	},
+	{
+		name: "Facebook",
+		icon: "/images/fb.png",
+		url: "#",
+	},
 ];
 
 const allCocktails = [
- {
-	id: 1,
-	name: "Classic Models",
-	image: "/images/drink1.png",
-	title: "Simple Workflows, Bold Ideas",
-	description:
-	 "Made with confidence, creativity, and passion.",
- },
- {
-	id: 2,
-	name: "Raspberry Mojito",
-	image: "/images/drink2.png",
-	title: "A Zesty Classic That Never Fails",
-	description:
-	 "The Margarita is a classic that balances tangy lime, smooth tequila, and a touch of sweetness. Shaken, frozen, or on the rocks—it’s always crisp & refreshing.",
- },
- {
-	id: 3,
-	name: "Violet Breeze",
-	image: "/images/drink3.png",
-	title: "Simple Ingredients, Bold Flavor",
-	description:
-	 "Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.",
- },
- {
-	id: 4,
-	name: "Curacao Mojito",
-	image: "/images/drink4.png",
-	title: "Crafted With Care, Poured With Love",
-	description:
-	 "Each cocktail is made with fresh ingredients and a passion for perfecting every pour, whether you're celebrating or simply relaxing.",
- },
+	{
+		id: 1,
+		name: "Classic Models",
+		image: "/images/drink1.png",
+		title: "Simple Workflows, Bold Ideas",
+		description:
+			"Made with confidence, creativity, and passion.",
+	},
+	{
+		id: 2,
+		name: "Raspberry Mojito",
+		image: "/images/drink2.png",
+		title: "A Zesty Classic That Never Fails",
+		description:
+			"The Margarita is a classic that balances tangy lime, smooth tequila, and a touch of sweetness. Shaken, frozen, or on the rocks—it’s always crisp & refreshing.",
+	},
+	{
+		id: 3,
+		name: "Violet Breeze",
+		image: "/images/drink3.png",
+		title: "Simple Ingredients, Bold Flavor",
+		description:
+			"Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.",
+	},
+	{
+		id: 4,
+		name: "Curacao Mojito",
+		image: "/images/drink4.png",
+		title: "Crafted With Care, Poured With Love",
+		description:
+			"Each cocktail is made with fresh ingredients and a passion for perfecting every pour, whether you're celebrating or simply relaxing.",
+	},
 ];
 
 export {
- navLinks,
- cocktailLists,
- mockTailLists,
- profileLists,
- featureLists,
- goodLists,
- openingHours,
- storeInfo,
- socials,
- allCocktails,
+	navLinks,
+	cocktailLists,
+	mockTailLists,
+	profileLists,
+	featureLists,
+	goodLists,
+	openingHours,
+	storeInfo,
+	socials,
+	allCocktails,
 };
